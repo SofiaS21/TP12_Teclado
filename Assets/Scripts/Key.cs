f8e47bfe3fc5be5a96d1697d5a985f69673e6757
SofiaS21 <49876490@est.ort.edu.ar>
2026-05-15 14:36:07 -0300

    Cambios en code

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    bool pressed = false;
+    float pressedOffset = 0.5f;
+
     void OnMouseDown(){
         Debug.Log("Click en " + gameObject.name);
+        transform.Translate(0,-pressedOffset,0);
+        pressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        if(pressed){
+            transform.Translate(0,pressedOffset,0);
+            pressed = false;
+        }
+    }
+
+     void OnMouseExit()
+    {
+        if(pressed){
+            transform.Translate(0,pressedOffset,0);
+            pressed = false;
+        }
     }
 }
