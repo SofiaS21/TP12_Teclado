2317ea3f8a508b95cece2475d5b138098c10a70e
SofiaS21 <49876490@est.ort.edu.ar>
2026-05-18 10:42:16 -0300

    Key board manager
    Agregue para que las teclas se definan y empeze con la consigna.

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -28,4 +28,5 @@ public class Key : MonoBehaviour
             pressed = false;
         }
     }
+
 }
---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public Key[] keys;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys = GameObject.FindObjectsOfType<Key>();
+    }
+
+    void DestroyKey()
+    {
+        int randomIndex
+
+        Destroy(keys[randomKey].gameObject);
+    }
+}
